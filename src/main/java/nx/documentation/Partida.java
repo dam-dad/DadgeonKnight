ffc04d9ac8c2bff4a20cf9bad2bdd17ad54190fa
cast.adree <andreacastrobonilla@gmail.com>
2023-03-02 14:09:29 +0000

    informe

---
+++ src/main/java/nx/documentation/Partida.java
@@ -0,0 +1,24 @@
+package nx.documentation;
+
+public class Partida {
+
+	private String nombre;
+	private String tiempo;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTiempo() {
+		return tiempo;
+	}
+
+	public void setTiempo(String tiempo) {
+		this.tiempo = tiempo;
+	}
+
+}
